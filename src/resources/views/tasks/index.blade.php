5cfa80763735a6f320b19fdaa5cf368690d57541
n-thanh <thban2803@gmail.com>
2022-03-15 16:39:30 +0900

    laravel project initial

---
+++ src/resources/views/tasks/index.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Todo App</title>
+    <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+    <header>
+        <nav class="my-navbar">
+            <a class="my-navbar-brand" href="/">Todo App</a>
+        </nav>
+    </header>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col col-md-4">
+                    <nav class="panel panel-default">
+                        <div class="panel-heading">
+                            <div class="panel-body">
+                                <a href="#" class="btn btn-default btn-block">
+                                Add Folders
+                                </a>
+                            </div>
+                            <div class="list-group">
+                                @foreach($folders as $folder)
+                                    <a href="{{ route('tasks.index', ['id' => $folder->id]) }}" class="list-group-item {{ $current_folder_id === $folder->id ? 'active' : '' }}"> 
+                                        {{ $folder->title }}
+                                    </a>
+                                @endforeach
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+                <div class="column col-md-8">
+
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
